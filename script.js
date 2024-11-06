2b193612cf2be14cb7fc24a5cbeefbcd3975c488
Sophie Usherwood <seufolds6@gmail.com>
2024-11-06 01:25:41 -0500

    Show new picture and play audio after clicking the first present, then go back to the present image page

--- script.js
+++ script.js
@@ -9,10 +9,32 @@ function startGame() {
     document.getElementById('game-screen').style.display = 'block';
 }
 
-function presentVideo(video_num) {
-    if (video_num == 1) {
-        alert("hi");
+function presentVideo(videoNum) {
+    const presentsImage = document.getElementById('presents');
+    let presentAudio = '';
+    let videoPicture = '';
+    console.log(videoNum);
+    
+    if (videoNum == 1) {
+        videoPicture = document.getElementById("present-1-picture");
+        presentAudio = 'present_audio/CFAA.m4a';
+        alert("Are you ready to join Noelle, Pim, and Pom?")
     }
+
+    videoPicture.style.display = "block";
+    presentsImage.style.display = "none";
+
+    // Hide all buttons with the class 'present'
+    const presentButtons = document.querySelectorAll('.present');
+    presentButtons.forEach(button => button.style.display = 'none');
+
+    const audio = new Audio(presentAudio);
+    audio.play();
+
+    audio.addEventListener('ended', () => {
+        videoPicture.style.display = "none";
+        presentsImage.style.display = "block";
+    });
 }
 
 // Initialize screens
